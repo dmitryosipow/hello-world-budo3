e87f2b7e1d764a16a266b0b6e8d02d87d6e3fa84
dmitryosipow <dimaosipow@yahoo.com>
2021-12-22 21:00:18 +0300

    Guess game init

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,33 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    private Vector2 v2;
-    public float speed;
+    [SerializeField] private int min;
+    [SerializeField] private int max;
+    private int guess;
     
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        Debug.Log($"Guess number from {min} to {max}");
+        CalculateGuess();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log($"GetButtonDown Frame {Time.frameCount}");
+            max = guess;
+            CalculateGuess();
         }
         
-        if (Input.GetButton("Jump"))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log($"GetButton Frame {Time.frameCount}");
+            min = guess;
+            CalculateGuess();
         }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Victory");
+        }
+    }
 
-        Vector3 pos = transform.position;
-        pos.x += move;
+    private void CalculateGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log($"Is your number {guess}");
     }
 }
